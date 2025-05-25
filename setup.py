31073140e24652a1487cd538e5c9c528253b0a93
AdityaKrishnanNeelakantan <aneela@asu.edu>
2025-05-25 01:15:18 -0700

    25

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-New file created
+import setuptools
+
+with open("README.md","r",encoding="utf8") as f:
+  long_description = f.read()
+  
+  __version__="0.0.0"
+  
+  REPO_NAME="End-to-End Text Summarizer"
+  AUT
